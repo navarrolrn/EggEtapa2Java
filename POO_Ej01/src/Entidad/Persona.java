6103e64889ed7f6d33597c8c08d7fdaa22a688fe
Leonardo <navarrolr@gmail.com>
2023-03-28 16:34:19 -0300

    Ofic 28/3 14:34

---
+++ POO_Ej01/src/Entidad/Persona.java
@@ -0,0 +1,19 @@
+/*
+ */
+package Entidad;
+
+
+public class Persona {
+    public String nombre, apellido;
+    public int edad;
+
+    public Persona(String nombre, String apellido, int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+
+    public Persona() {
+    }
+
+}
---
+++ POO_Ej01/src/poo_ej01/POO_Ej01.java
@@ -0,0 +1,16 @@
+/*
+ */
+package poo_ej01;
+
+import Entidad.Persona;
+        
+public class POO_Ej01 {
+
+
+    public static void main(String[] args) {
+        Persona primeraPersona=new Persona();
+        Persona segundaPersona=new Persona("Pedro","Perez",26);
+    }
+
+    
+}
